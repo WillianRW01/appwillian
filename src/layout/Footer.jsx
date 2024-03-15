9b6d1c8a12e368e16340adc73a998f2139a37c18
Your Name <you@example.com>
2024-03-14 21:44:34 -0300

    inicial

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,31 @@
+import { Container } from "react-bootstrap";
+
+export default function Footer ()
+{
+    return(
+   <Container>
+   <Row>
+    <Col lg={4}  sm={12} >
+    <div>
+    <h4>Sobre minha empresa</h4>
+    <p>Av Visconde de Taunay 777777</p>    
+        
+    </div>    
+    </Col>
+    <Col lg = {8} sm={12} > 
+    <div>
+            <h4>Redes sociais</h4>
+            <p>
+              <a href="//facebook.com" target="_blank">
+                Facebook
+              </a>
+            </p>
+            <p>Instagram</p>
+          </div>
+    
+    </Col>
+   </Row>
+
+   </Container>
+);
+}
